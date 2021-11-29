710156aac8e2743fbb7a0dd860f4e44725b53a32
daddyUrsa <0057747@gmail.com>
2021-11-29 12:32:24 +0700

    Initial Commit

---
+++ URLSessionStartProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  URLSessionStartProject
+//
+//  Created by Alexey Pavlov on 29.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
